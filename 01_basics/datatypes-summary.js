438a149f279722d0012562946ac75e8758c7db93
Soumyajeet Bal <66509457+soumyajeet-xo@users.noreply.github.com>
2023-11-01 21:16:16 +0530

    Update datatypes-summary.js

--- 01_basics/datatypes-summary.js
+++ 01_basics/datatypes-summary.js
@@ -9,22 +9,22 @@ const isLoggedIn = false
 const outsideTemp = null
 let userEmail;
 
-const id = Symbol('123')
-const anotherId = Symbol('123')
+const id = Symbol('123')  // type is symbol itself.
+const anotherId = Symbol('123')  // both are not same
 
 console.log(id === anotherId);
 
 // const bigNumber = 3456543576654356754n
 
+objects and web events... 
 
-
-// Reference (Non primitive)
+// Reference (Non-primitive)
 
 // Array, Objects, Functions
 
-const heros = ["shaktiman", "naagraj", "doga"];
+const heros = ["shaktiman", "naagraj", "doga"]; // array
 let myObj = {
-    name: "hitesh",
+    name: "hitesh",  //objects // typeof is function
     age: 22,
 }
 
@@ -33,5 +33,7 @@ const myFunction = function(){
 }
 
 console.log(typeof anotherId);
+
+// type of big int is null
 
 // https://262.ecma-international.org/5.1/#sec-11.4.3
