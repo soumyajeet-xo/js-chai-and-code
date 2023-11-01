bb9e1c7c65f9525d2575c1367a41f972f75f5b19
Soumyajeet Bal <66509457+soumyajeet-xo@users.noreply.github.com>
2023-11-01 22:23:44 +0530

    Update 05_strings.js

--- 01_basics/05_strings.js
+++ 01_basics/05_strings.js
@@ -2,7 +2,7 @@ const name = "hitesh"
 const repoCount = 50
 
 // console.log(name + repoCount + " Value");
-
+// backticks and **string interpolation
 console.log(`Hello my name is ${name} and my repo count is ${repoCount}`);
 
 const gameName = new String('hitesh-hc-com')
